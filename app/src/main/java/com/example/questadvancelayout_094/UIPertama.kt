50d8861dad7d4ab2b8f9505f6c3b806676e4aac9
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-23 11:36:14 +0700

    Menambahkan teks judul utama untuk layar program studi (prodi).Teks diambil dari R.string.prodi

--- app/src/main/java/com/example/questadvancelayout_094/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_094/UIPertama.kt
@@ -1,6 +1,8 @@
 package com.example.questadvancelayout_094
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @composable
@@ -9,6 +11,8 @@ fun ActivitasPertama(modifier: Modifier){
         .fillmaxsize(),
         horizontalAlignment = Alignment.CenterHorizontally)
     ){
-
+        Text(text= stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
     }
 }
