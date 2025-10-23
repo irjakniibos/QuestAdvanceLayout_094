e561980985ecb2293e20a1d28c52e45a3d917202
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-23 11:56:38 +0700

    Menambahkan gambar dengan diambil dari R.drawable.LogoUMY

--- app/src/main/java/com/example/questadvancelayout_094/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_094/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.questadvancelayout_094
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -36,7 +38,9 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )){
-
+            Row(){
+                val gambar = painterResource(R.drawable.LogoUMY)
+            }
         }
 
     }
