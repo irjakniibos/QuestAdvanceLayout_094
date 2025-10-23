9c7b10253b08e0dfd1aac4f4fe421a2677e47960
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-23 11:45:32 +0700

    Menambahkan teks nama univ untuk layar program .Teks diambil dari R.string.univ

--- app/src/main/java/com/example/questadvancelayout_094/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_094/UIPertama.kt
@@ -1,18 +1,31 @@
 package com.example.questadvancelayout_094
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-@composable
+@Composable
 fun ActivitasPertama(modifier: Modifier){
     Column(modifier = Modifier.padding(top=100.dp)
-        .fillmaxsize(),
-        horizontalAlignment = Alignment.CenterHorizontally)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Text(text= stringResource(id = R.string.prodi),
+        Text(stringResource(R.string.prodi),
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold)
+        Text(stringResource(R.string.univ),
+            fontSize = 22.sp)
+        Spacer(modifier = Modifier.height(25.dp))
+
     }
 }
