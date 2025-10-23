5729dd4d8cfb9360c6624d59ae2d22137663c337
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-23 11:59:24 +0700

    Menambahkan komponen Image dengan painter 'gambar'. Ukuran gambar diatur ke 100.dp dan ditambahkan padding 5.dp di sekelilingnya.Content description diatur ke null karena gambar dianggap dekoratif.

--- app/src/main/java/com/example/questadvancelayout_094/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_094/UIPertama.kt
@@ -1,5 +1,6 @@
 package com.example.questadvancelayout_094
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -40,6 +42,11 @@ fun ActivitasPertama(modifier: Modifier){
             )){
             Row(){
                 val gambar = painterResource(R.drawable.LogoUMY)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
             }
         }
 
